255163d0e8080fd115c01d9ca84cb8f496127dee
A Potašenkov <a.potasenkov@voco.ee>
2023-11-01 15:02:02 +0200

    Displayed all articles

---
+++ Joga/index.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+
+const path = require('path')
+
+//add template engine 
+const hbs = require('express-handlebars');
+// setup template directory and files extensions
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','hbs');
+app.engine('hbs', hbs.engine ({
+  extname: 'hbs',
+  defaultLayout: 'main',
+  layoutsDir:__dirname+'/views/layouts/',
+}))
+
+// setup static public directory
+app.use(express.static('public'));
+
+const mysql = require('mysql')
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({extended: true}))
+
+// create database connection
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: 'root',
+  password: 'qwerty',
+  database: 'joga_mysql'
+})
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("connected to joga mysql");
+})
+
+//app start point
+
+app.get('/',(req,res) => {
+  let query = 'SELECT * FROM article';
+  let articles = []
+  con.query(query, (err,result) =>{
+    if (err) throw err;
+    articles = result
+    res.render('index',{
+      articles: articles
+    } ) 
+    //console.log(articles)
+
+  })
+  
+});
+
+app.listen(3000, () => {
+  console.log('app is started at https://localhost:3000');
+});
